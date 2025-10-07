6b0779df90a5b926c00c41bdffa8c5a48ef70cca
Alicia <aliciasanchez11614@gmail.com>
2025-10-07 13:16:29 -0700

    part 2 of Lab 3

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 
 const app = express();
 const port = 8000;
@@ -46,6 +47,7 @@ const addUser = (user) => {
     return user;
 };
 
+app.use(cors());
 
 app.use(express.json());
 
@@ -65,7 +67,7 @@ app.get("/users", (req, res) => {
         result = result.filter((user) => user.job === job);
 
     }
-    res.send(result);
+    res.send({users_list: result});
 });
 
 app.get("/users/:id", (req, res) => {
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,21 +1,33 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Table from "./Table";
 import Form from "./Form"
 
 
 function MyApp(){
-    const[characters, setCharacter] = useState([]);
+    const[characters, setCharacters] = useState([]);
 
     function removeOneCharacter(index){
         const updated = characters.filter((character, i) => {
             return i !== index;
         });
-        setCharacter(updated);
+        setCharacters(updated);
     }
 
     function updateList(person){
-        setCharacter([...characters, person]);
+        setCharacters([...characters, person]);
     }
+
+    function fetchUsers(){
+        const promise = fetch("http://localhost:8000/users");
+        return promise;
+    }
+
+    useEffect(() => {
+        fetchUsers()
+                .then((res) => res.json())
+                .then((json) => setCharacters(json["users_list"]))
+                .catch((error) => { console.log(error); });
+    }, []);
 
 
     return(
