e6d1546d2d3537eeb0cac921a41c6d871c3e7d4a
Alicia <aliciasanchez11614@gmail.com>
2025-10-07 22:57:15 -0700

    Frontend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -6,19 +6,58 @@ import Form from "./Form"
 function MyApp(){
     const[characters, setCharacters] = useState([]);
 
-    function removeOneCharacter(index){
-        const updated = characters.filter((character, i) => {
-            return i !== index;
-        });
-        setCharacters(updated);
+    function removeOneCharacter(id){
+        //call fetch
+        fetch(`http://localhost:8000/users/${id}`, {
+            method: "DELETE",
+        })
+
+        //wait for promise
+        .then((res) => {
+            if (res.status === 204) { //Check for successful delete
+                //filter by id
+                const updated = characters.filter((character) => character.id !== id);
+                setCharacters(updated);
+            } else if (res.status === 404){
+                console.log("User not found.");
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     }
 
     function updateList(person){
-        setCharacters([...characters, person]);
+        //Person will only have name and job
+        postUser(person)
+            .then((response) => {
+                if (response.status === 201){
+                    return response.json();
+                } else {
+                    console.log("User not created. Server returned:", response.status);
+                }
+            })
+            //new user now has id so use instead of person
+            .then((newUser) => setCharacters([...characters, newUser]))
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
     function fetchUsers(){
         const promise = fetch("http://localhost:8000/users");
+        return promise;
+    }
+
+    function postUser(person) {
+        const promise = fetch("http://localhost:8000/users", {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(person),
+        });
+        
         return promise;
     }
 
